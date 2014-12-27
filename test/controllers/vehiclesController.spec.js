b482349d2d43e99d84e439ecf3da6935bda6d76e
Matt Sell <mwsell@gmail.com>
2014-12-27 12:20:39 -0600

    add one unimplemented test

---
+++ test/controllers/vehiclesController.spec.js
@@ -0,0 +1,6 @@
+var VehcileController = require('../../api/controllers/VehicleController');
+    //sinon = require('sinon'),
+
+describe("get vehicles", function(){
+    it("should not allow anonymous access");
+})
