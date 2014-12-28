f1ca6aa6f4e59caa601d55b01b1893752d19508c
Matt Sell <mwsell@gmail.com>
2014-12-28 11:41:55 -0600

    working on testing authentication

---
+++ test/controllers/authController.spec.js
@@ -0,0 +1,17 @@
+var VehcileController = require('../../api/controllers/AuthController'),
+    request = require('supertest'),
+    expect = require('expect'),
+    chai = require('chai');
+
+//sinon = require('sinon'),
+
+describe('AuthController', function () {
+    describe("register", function () {
+        it("should register new user", function (done) {
+            request(sails.hooks.http.app)
+                .get('/auth/register')
+                .send({email:'foo@bar.com', password:'abc123!'})
+                .expect(200, done);
+        });    
+    });
+});
--- test/controllers/vehicleController.spec.js
+++ test/controllers/vehicleController.spec.js
@@ -2,20 +2,29 @@ var VehcileController = require('../../api/controllers/VehicleController'),
     request = require('supertest'),
     expect = require('expect'),
     chai = require('chai');
-    
+
 //sinon = require('sinon'),
 
-describe('VehicleController', function () {    
+describe('VehicleController', function () {
     describe("get vehicles", function () {
         it("should not allow anonymous access", function (done) {
             request(sails.hooks.http.app)
                 .get('/vehicle')
-                .expect(401,done);
+                .expect(401, done);
         });
-        it("should allow user to post new vehicle", function(done){
+        it("should allow authenticated access", function (done) {
+            request(sails.hooks.http.app)
+                .get('/vehicle')
+                .set({
+                        'Authorization': 'Token 1234',
+                        'Contetn-Type': 'application/json'
+                     })
+                .expect(200, done);
+        });
+        it("should allow user to post new vehicle", function (done) {
             request(sails.hooks.http.app)
                 .post('/vehicle/create?make=Toyota&model=FJ+Cruiser')
-                .expect(200,done);
+                .expect(200, done);
         });
     });
 });
