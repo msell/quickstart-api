29181d605d60dd113bef1277ac55d4c51b4e1716
Matt Sell <mwsell@gmail.com>
2014-12-28 11:17:41 -0600

    user should be able to post new vehicle

--- test/controllers/vehicleController.spec.js
+++ test/controllers/vehicleController.spec.js
@@ -2,15 +2,20 @@ var VehcileController = require('../../api/controllers/VehicleController'),
     request = require('supertest'),
     expect = require('expect'),
     chai = require('chai');
+    
 //sinon = require('sinon'),
 
-describe('VehicleController', function () {
+describe('VehicleController', function () {    
     describe("get vehicles", function () {
         it("should not allow anonymous access", function (done) {
             request(sails.hooks.http.app)
                 .get('/vehicle')
                 .expect(401,done);
         });
-        it("should allow user to post new vehicle");
+        it("should allow user to post new vehicle", function(done){
+            request(sails.hooks.http.app)
+                .post('/vehicle/create?make=Toyota&model=FJ+Cruiser')
+                .expect(200,done);
+        });
     });
 });
