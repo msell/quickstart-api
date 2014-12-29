4ba95289520a9a9bab4ffad606f9d46e66fb6586
Matt Sell <mwsell@gmail.com>
2014-12-28 19:14:06 -0600

    test that authService returns token upon registation

--- test/controllers/authController.spec.js
+++ test/controllers/authController.spec.js
@@ -12,6 +12,27 @@ describe('AuthController', function () {
                 .post('/auth/register')
                 .send({email:'foo@bar.com', password:'abc123!'})
                 .expect(200, done);
-        });    
+        });
+        it("should reqire password", function (done) {
+            request(sails.hooks.http.app)
+                .post('/auth/register')
+                .send({email:'foo@bar.com'})
+                .expect(401, done);
+        });
+        it("should return auth token", function (done) {
+            request(sails.hooks.http.app)
+                .post('/auth/register')
+                .send({email:'baz@bar.com', password:'abc123!'})
+                .expect(200)
+                .expect(sentToken)
+                .end(function(err, res){
+                    if(err) return done(err);
+                    done();
+            })
+        });
+        
+        function sentToken(res){
+            if(!('token' in res.body)) throw new Error('missing token in body');
+        }
     });
 });
